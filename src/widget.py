3be3475a5acf19c614ce035a1b56a3f6b7d75349
kirill <dddk1697@gmail.com>
2026-09-15 12:54:37 +0300

    init github

---
+++ src/widget.py
@@ -0,0 +1,19 @@
+from typing import final
+from masks import get_mask_card_number, get_mask_account
+
+
+def mask_account_card(card_type_number: str) -> str:
+    card_list = card_type_number.split()
+    card_number: str = card_list[-1]
+    card_type = " ".join(card_list[:-1])
+    if card_type == "Счет":
+        card_type_number_mask = f"{card_type} {get_mask_account(card_number)}"
+        return card_type_number_mask
+    else:
+        card_type_number_mask = f"{card_type} {get_mask_card_number(card_number)}"
+        return card_type_number_mask
+
+
+def get_date(date_input: str) -> str:
+    date = f"{date_input[8:10]}.{date_input[5:7]}.{date_input[:4]}"
+    return date
