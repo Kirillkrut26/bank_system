1f395d1f2bcbb30564129ddd2dc7e3db5bc3df81
kirill <dddk1697@gmail.com>
2026-09-17 11:11:55 +0300

    Bug fixes

--- src/processing.py
+++ src/processing.py
@@ -1,13 +1,12 @@
-from unittest import result
-
-
-def filter_by_state(list_dictionaries, state="EXECUTED"):
+def filter_by_state(list_dictionaries: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """ "возвращает новый список словарей, содержащий только те словари, у которых ключ state"""
     result = []
-    for i in list_dictionaries:
-        if i["state"] == state:
-            result.append(i)
+    for temporary_list_dictionaries in list_dictionaries:
+        if temporary_list_dictionaries["state"] == state:
+            result.append(temporary_list_dictionaries)
     return result
 
 
-def sort_by_date(list_dictionaries, reverse=True):
-    return sorted(list_dictionaries, key=lambda i: i["date"], reverse=reverse)
+def sort_by_date(list_dictionaries: list[dict], reverse: bool = True) -> list[dict]:
+    """возвращает новый список, отсортированный по дате"""
+    return sorted(list_dictionaries, key=lambda item: item["date"], reverse=reverse)
