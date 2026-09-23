b20da7b04563b437188002e6e45dba2d62e5e61c
kirill <dddk1697@gmail.com>
2026-09-23 15:38:32 +0300

    add test_processing.py

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,58 +1,62 @@
 from src.processing import filter_by_state, sort_by_date
+import pytest
 
 
-def test_filter_by_state():
-    assert filter_by_state([{"id": 414288290, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"}]) == [
-        {"id": 414288290, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"}
-    ]
-    assert (
-        filter_by_state([{"id": 414288290, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"}], "CANCELED")
-        == []
-    )
-    assert filter_by_state(
-        [{"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}], "CANCELED"
-    ) == [{"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}]
-
-
-def test_sort_by_date():
-    assert sort_by_date(
-        [
-            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-        ],
-        True,
-    ) == [
+@pytest.fixture
+def list_dict():
+    return [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
     ]
-    assert sort_by_date(
-        [
-            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-        ],
-        False,
-    ) == [
+
+
+@pytest.fixture
+def value_canceled_list_dict():
+    return [
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
+
+@pytest.fixture
+def value_executed_list_dict():
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
+
+def test_filter_by_state(list_dict, value_canceled_list_dict, value_executed_list_dict):
+    assert filter_by_state(list_dict, "CANCELED") == value_canceled_list_dict
+    assert filter_by_state(list_dict, "EXECUTED") == value_executed_list_dict
+
+
+def test_empty_filter_by_state(list_dict, value_canceled_list_dict, value_executed_list_dict):
+    assert filter_by_state(list_dict, "") == []
+
+
+@pytest.fixture
+def true_sort_by_date():
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
+
+@pytest.fixture
+def false_sort_by_date():
+    return [
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
     ]
-    assert sort_by_date(
-        [
-            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-        ]
-    ) == [
-        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-    ]-
+
+
+def test_sort_by_date(list_dict, true_sort_by_date, false_sort_by_date):
+    assert sort_by_date(list_dict, False) == false_sort_by_date
+    assert sort_by_date(list_dict, True) == true_sort_by_date
