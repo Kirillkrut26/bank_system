70ccfe96e6e15fc4e2fb91f812437c39647942d6
kirill <dddk1697@gmail.com>
2026-09-23 13:05:44 +0300

    complete test_masks.py

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,15 +1,37 @@
 from src.masks import get_mask_card_number, get_mask_account
+import pytest
 
 
-def test_get_mask_card_number():
-    assert get_mask_card_number("1234123412341234") == "1234 12** **** 1234"
-    assert get_mask_card_number("12341234123412341234") == "Неверный номер карты"
-    assert get_mask_card_number("1234") == "Неверный номер карты"
-    assert get_mask_card_number("") == "Неверный номер карты"
+@pytest.fixture
+def valid_card_number():
+    return "1234123412341234"
+
+def test_get_mask_card_number(valid_card_number):
+    assert get_mask_card_number(valid_card_number) == "1234 12** **** 1234"
+
+@pytest.mark.parametrize(
+    "bad_card_number",
+    [
+        "12341234123412341234",
+        "1234",
+        ""
+    ],
+)
+
+def test_error_get_mask_card_number(bad_card_number):
+    with pytest.raises(ValueError):
+        get_mask_card_number(bad_card_number)
 
 
-def test_get_mask_account():
-    assert get_mask_account("123456789123456789") == "** 6789"
-    assert get_mask_account("123456") == "** 3456"
-    assert get_mask_account("1234123456789456789123456") == "** 3456"
-    assert get_mask_account("12") == "** 12"
+@pytest.mark.parametrize(
+    "number_check, mask_number_check",
+    [
+        ("123456789123456789", "** 6789"),
+        ("123456", "** 3456"),
+        ("1234123456789456789123456", "** 3456"),
+        ("12", "** 12"),
+    ],
+)
+
+def test_get_mask_account(number_check, mask_number_check):
+    assert get_mask_account(number_check) == mask_number_check
