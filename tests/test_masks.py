025b70571abd4ac732b670bc6f66ea4821c15dd0
kirill <dddk1697@gmail.com>
2026-09-23 13:08:28 +0300

    edit for flacke8 test_masks.py

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -6,18 +6,15 @@ import pytest
 def valid_card_number():
     return "1234123412341234"
 
+
 def test_get_mask_card_number(valid_card_number):
     assert get_mask_card_number(valid_card_number) == "1234 12** **** 1234"
 
+
 @pytest.mark.parametrize(
     "bad_card_number",
-    [
-        "12341234123412341234",
-        "1234",
-        ""
-    ],
+    ["12341234123412341234", "1234", ""],
 )
-
 def test_error_get_mask_card_number(bad_card_number):
     with pytest.raises(ValueError):
         get_mask_card_number(bad_card_number)
@@ -32,6 +29,5 @@ def test_error_get_mask_card_number(bad_card_number):
         ("12", "** 12"),
     ],
 )
-
 def test_get_mask_account(number_check, mask_number_check):
     assert get_mask_account(number_check) == mask_number_check
